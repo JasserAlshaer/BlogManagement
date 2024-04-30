91345b0eb5fc22d9ac0d60fa1cf1351dc8d40671
ASUS <ASUS@DESKTOP-SV986PK>
2024-04-30 20:46:09 +0300

    blogManagement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './Pages/main-page/main-page.component';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { CreateBlogComponent } from './Pages/create-blog/create-blog.component';
+import { ManageBlogComponent } from './Pages/manage-blog/manage-blog.component';
+import { ErrorMsgComponent } from './Pages/error-msg/error-msg.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:MainPageComponent
+  },
+  {
+    path:'login',component:LoginPageComponent
+  },
+  {
+    path:'create',component:CreateBlogComponent
+  },
+  {
+    path:'manage',component:ManageBlogComponent
+  },
+  {
+    path:'**',
+    component:ErrorMsgComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './base-component/app.component';
+import { MainPageComponent } from './Pages/main-page/main-page.component';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { CreateBlogComponent } from './Pages/create-blog/create-blog.component';
+import { ManageBlogComponent } from './Pages/manage-blog/manage-blog.component';
+import { ErrorMsgComponent } from './Pages/error-msg/error-msg.component';
+import { NavbarComponent } from './Reusable/navbar/navbar.component';
+import { FooterComponent } from './Reusable/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LoginPageComponent,
+    CreateBlogComponent,
+    ManageBlogComponent,
+    ErrorMsgComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
